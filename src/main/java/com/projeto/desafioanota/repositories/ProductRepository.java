25c1a2e0898c69f8189d7690ea0d0d81919d16f9
barbararibeiroo <barbararibeiro18@gmail.com>
2024-04-16 00:02:23 -0300

    Create package repository and domain

--- src/main/java/com/projeto/desafioanota/repositories/ProductRepository.java
+++ src/main/java/com/projeto/desafioanota/repositories/ProductRepository.java
@@ -1,9 +1,9 @@
 package com.projeto.desafioanota.repositories;
 
-import com.projeto.desafioanota.domain.category.Category;
+import com.projeto.desafioanota.domain.product.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository extends MongoRepository<Category, String> {
+public interface ProductRepository extends MongoRepository<Product, String> {
 }
