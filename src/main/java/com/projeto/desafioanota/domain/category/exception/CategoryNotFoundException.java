bb518c626a5f5c208953f3f27742add333d31986
barbararibeiroo <barbararibeiro18@gmail.com>
2024-04-22 00:20:57 -0300

    Create exception

---
+++ src/main/java/com/projeto/desafioanota/domain/category/exception/CategoryNotFoundException.java
@@ -0,0 +1,4 @@
+package com.projeto.desafioanota.domain.category.exception;
+
+public class CategoryNotFoundException extends RuntimeException {
+}
---
+++ src/main/java/com/projeto/desafioanota/domain/product/exception/ProductNotFoundException.java
@@ -0,0 +1,4 @@
+package com.projeto.desafioanota.domain.product.exception;
+
+public class ProductNotFoundException extends RuntimeException{
+}
